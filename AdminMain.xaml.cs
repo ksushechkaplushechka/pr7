6fb7ff48323e87c65d0e4049da85f80ccd52fcb7
Porfireva <isip_k.d.porfiryeva@mpt.ru>
2024-12-19 22:23:38 +0300

    helooooooooooooo

--- AdminMain.xaml.cs
+++ AdminMain.xaml.cs
@@ -7,7 +7,6 @@ using Microsoft.Data.Sqlite;
 namespace Autobike
 {
     /// <summary>
-    /// Логика взаимодействия для AdminMain.xaml
     /// </summary>
     public partial class AdminMain : Window
     {
@@ -15,85 +14,71 @@ namespace Autobike
         {
             InitializeComponent();
         }
-
         private void CarsButton_Click(object sender, RoutedEventArgs e)
         {
             CarWindow carWindow = new CarWindow();
             carWindow.Show();
         }
-
         private void CarBrandButton_Click(object sender, RoutedEventArgs e)
         {
             CarBrandWindow carBrandWindow = new CarBrandWindow();
             carBrandWindow.Show();
         }
-
         private void MaintenceButton_Click(object sender, RoutedEventArgs e)
         {
             MaintenanceWindow maintenanceWindow = new MaintenanceWindow();
             maintenanceWindow.Show();
         }
-
         private void InsurenceButton_Click(object sender, RoutedEventArgs e)
         {
             InsuranceWindow insuranceWindow = new InsuranceWindow();
             insuranceWindow.Show();
         }
-
         private void FineButton_Click(object sender, RoutedEventArgs e)
         {
             FineWindow fineWindow = new FineWindow();
             fineWindow.Show();
         }
-
         private void DepartmentButton_Click(object sender, RoutedEventArgs e)
         {
             DepartmentWindow departmentWindow = new DepartmentWindow();
             departmentWindow.Show();
         }
-
         private void TariffButton_Click(object sender, RoutedEventArgs e)
         {
             TariffWindow tariffWindow = new TariffWindow();
             tariffWindow.Show();
         }
-
         private void EmployeeButton_Click(object sender, RoutedEventArgs e)
         {
             EmployeeWindow employeeWindow = new EmployeeWindow();
             employeeWindow.Show();
         }
-
         private void CLientButton_Click(object sender, RoutedEventArgs e)
         {
             ClientWindow clientWindow = new ClientWindow();
             clientWindow.Show();
         }
-
         private void AccountButton_Click(object sender, RoutedEventArgs e)
         {
             AccountWindow accountWindow = new AccountWindow();
             accountWindow.Show();
         }
-
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
         private void ChartButton_Click(object sender, RoutedEventArgs e)
         {
-            // Открытие окна с графиком
             BookingChartWindow chartWindow = new BookingChartWindow();
             chartWindow.Show();
         }
-
         private void ExitButton_Click(object sender, RoutedEventArgs e)
         {
             LoginWindow loginWindow = new LoginWindow();
             this.Close();
             loginWindow.Show();
         }
-
         private void ExportWithoutDataButton_Click(object sender, RoutedEventArgs e)
         {
             try
@@ -109,10 +94,8 @@ namespace Autobike
                 {
                     string destinationPath = saveFileDialog.FileName;
 
-                    // Указываем полный путь к файлу базы данных
-                    string databasePath = @"C:\Path\To\Database\AutocarsData.db"; // Укажите точный путь
+                    string databasePath = @"C:\\Path\\To\\Database\\AutocarsData.db"; // Укажите точный путь
 
-                    // Проверяем, существует ли файл
                     if (!File.Exists(databasePath))
                     {
                         MessageBox.Show($"Файл базы данных не найден по пути: {databasePath}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
@@ -125,7 +108,6 @@ namespace Autobike
                         sourceConnection.Open();
                         destinationConnection.Open();
 
-                        // Копируем структуру таблиц из исходной базы данных в целевую
                         using (var command = sourceConnection.CreateCommand())
                         {
                             command.CommandText = "SELECT sql FROM sqlite_master WHERE type='table';";
@@ -152,10 +134,8 @@ namespace Autobike
                 MessageBox.Show($"Ошибка при экспорте базы данных без данных: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
-
         private void ExportWithDataButton_Click(object sender, RoutedEventArgs e)
         {
-
         }
     }
 }
